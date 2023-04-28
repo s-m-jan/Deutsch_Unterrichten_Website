c77cac4252873d2386d02e2ed94b0233429c43ec
Mehdi <mehdi.janfeshan2014@gmail.com>
2023-04-28 14:03:28 +0430

    responsive initiated

--- Js Files/script.js
+++ Js Files/script.js
@@ -10,5 +10,20 @@ setInterval(() => {
         }
 }, 4000);
 
-
+let welcomeMessage = document.querySelector(".welcome-messages-item")
+let welcomeMessages = [
+  
+ "تیم آموزشی اقبال تا اخذ مدرک بین المللی زبان آلمانی در کنارتان خواهد بود",
+  "آموزش آلمانی با جدیدترین فنون و متدهای روز دنیا",
+  "به وبسایت شخصی استاد فریبا اقبال خوش آمدید"
+];
+let j = 0;
+setInterval(() => {
+    welcomeMessage.innerText = welcomeMessages[j]
+    j++
+    if(j=== 3){
+        j=0
+    }
+}, 7000);
+//end: Header scripts//
 
